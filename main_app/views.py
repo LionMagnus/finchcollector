a8afe1a54b6196f6f51911c036886c80b5ecc898
LionMagnus <xinahoshinx@gmail.com>
2022-11-17 02:55:27 -0500

    part4 lab

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
+from .forms import FeedingForm
 
 # Create your views here.
 def home(request):
@@ -16,6 +18,35 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
+  feeding_form = FeedingForm()
   return render(request, 'finches/detail.html', {
-    'finch': finch
+    'finch': finch, 'feeding_form': feeding_form
   })
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = '__all__'
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  # Let's disallow the renaming of a cat by excluding the name field!
+  fields = ['gender', 'description', 'age']
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches'
+
+def add_feeding(request, finch_id):
+  # create a ModelForm instance using 
+  # the data that was submitted in the form
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # We want a model instance, but
+    # we can't save to the db yet
+    # because we have not assigned the
+    # finch_id FK.
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('detail', finch_id=finch_id)
